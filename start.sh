324d7821c4f69b9f4c7e16fce9de2c236ac81822
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-11-22 22:09:40 +0100

    ..

---
+++ start.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+../wait-for-it/wait-for-it.sh $RABBITMQ_URL -t 0
+../wait-for-it/wait-for-it.sh $(echo "$REMOULADE_API" | sed -e "s/[^/]*\/\/\([^@]*@\)\?\([^:/]*\)\(:\([0-9]\{1,5\}\)\)\?.*/\2\3/") -t 0
+
+_term() {
+  echo "Caught SIGTERM signal!"
+  kill -TERM "$child" 2>/dev/null
+}
+trap _term SIGTERM
+
+set -xv
+if $WATCHMEDO; then
+  watchmedo auto-restart --debounce-interval 1 --interval $WATCHMEDO_INTERVAL -d .  --patterns="*.py;*.egg" --recursive  -- ./start2.sh  &
+else
+  ./start2.sh  &
+fi
+child=$!
+wait "$child"
+echo "end"
+
+
+
+
+
